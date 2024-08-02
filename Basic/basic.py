19bf269ae5d0b0fa5c5e3e69980f4dbd55be45b9
Rohitbule <rohitbule539@gmail.com>
2024-08-02 05:40:14 +0530

    Basic python

---
+++ Basic/basic.py
@@ -0,0 +1,7 @@
+#print("hello python")
+
+Name = "Rohit"
+#age = int(input("Age is : "))
+#Price = float(input("Price is : "))
+#print("Name is :",Name, "\nAge is :",age,"\nPrice is :",Price)
+#print(type(Name))
